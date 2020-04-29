73fc0985e26e60bf26321be667b0446957f54d4b
Sarah Shatto <Sarahshatto@gmail.com>
2020-04-29 00:34:43 -0700

    added information to .index.html and app.js

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+'use strict';
+
+
+
+alert('Welcome! please answer a few questions.');
+
+// First Question
+var FirstTime = prompt('Have you visited this website before? y/n').toLowerCase;
+
+  console.log('user has or has not visited =' + FirstTime);
+
+
+
+// var Office1 = prompt('Do you love the Office , ' + name + ' ? ');
+
+// console.log(Office1);
+
+// alert('Im glad you love the Office ' + name + '.');
+
+// var Office2 = prompt('Question. What kind of bear is best?');
+
+// console.log(Office2);
+
+// alert(Office2 + ' is False. Black Bear.');
+
+// var Office3 = prompt( 'True or False. Bears eat beets.');
+
+// console.log(Office3);
+
+// alert('IDENTITY THEFT IS NOT A JOKE, ' + name + '!');
+
